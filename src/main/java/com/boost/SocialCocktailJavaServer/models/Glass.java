b58dc82beacebc7ffb62057110259dd2de5d3854
thomas-vaiarella <thomasvaiarella@gmail.com>
2019-06-18 20:13:24 -0400

    Support for glasses

--- src/main/java/com/boost/SocialCocktailJavaServer/models/Glass.java
+++ src/main/java/com/boost/SocialCocktailJavaServer/models/Glass.java
@@ -1,11 +1,15 @@
 package com.boost.SocialCocktailJavaServer.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonView;
+
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Glass {
@@ -15,6 +19,34 @@ public class Glass {
 
     private String name;
     private String description;
-
     
+    @OneToMany
+    @JsonIgnore
+    private List<Cocktail> cocktails;
+    
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public List<Cocktail> getCocktails() {
+		return cocktails;
+	}
+	public void setCocktails(List<Cocktail> cocktails) {
+		this.cocktails = cocktails;
+	}
+
 }
